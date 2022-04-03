2fe6b30ce8f04721db57b75ded1fac17af5159a4
Your Name <you@example.com>
2022-04-03 12:28:04 +0430

    new permission method

--- app/src/main/java/com/example/taraziot/MainActivity.java
+++ app/src/main/java/com/example/taraziot/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,10 +32,15 @@ import androidx.core.content.ContextCompat;
 
 import com.example.taraziot.receiver.SMSReceiverImpl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "TAG";
     Button btnRefresh, b1, stop, vibrate, startService, stopService, stopSMS, startSMS, configServerBtn, statusRefreshButton, armAlarmButton, disarmAlarmButton, disableAlarmSoundButton;
     public static TextView smsNumberText, statusTxt, armedStatusTxt;
     EditText text;
@@ -47,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     UserManagerSharedPrefs userManagerSharedPrefs;
     String destinationAddress, statusFromServer;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 
 //    SMSReceiverImpl smsReceiver = new SMSReceiverImpl();
 
@@ -75,7 +82,9 @@ public class MainActivity extends AppCompatActivity {
         disarmAlarmButton = findViewById(R.id.disarmAlarmButton);
         disableAlarmSoundButton = findViewById(R.id.disableAlarmSoundButton);
         configServerBtn = findViewById(R.id.configServerBtn);
-destinationAddress = "9127938973";
+//destinationAddress = "9127938973";
+destinationAddress = "9944420283";
+checkAndRequestPermissions();
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.SEND_SMS)) {
@@ -882,27 +891,126 @@ destinationAddress = "9127938973";
 
     }
 
+    private  boolean checkAndRequestPermissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int receiveSMSPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        int readSMSPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (receiveSMSPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.RECEIVE_SMS);
+        }
+        if (readSMSPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_SMS);
+        }
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+
     @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        if (requestCode == SMS_REQUEST_CODE) {
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
 
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initialize the map with both permissions
+                perms.put(Manifest.permission.SEND_SMS, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.RECEIVE_SMS, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.READ_SMS, PackageManager.PERMISSION_GRANTED);
+                // Fill with actual results from user
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++)
+                        perms.put(permissions[i], grantResults[i]);
+                    // Check for both permissions
+                    if (perms.get(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED && perms.get(Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "sms & location services permission granted");
+                        // process the normal flow
+                        //else any one or both the permissions are not granted
+                    } else {
+                        Log.d(TAG, "Some permissions are not granted ask again ");
+                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+//                        // shouldShowRequestPermissionRationale will return true
+                        //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS) ||
+                        ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)
+                        ) {
+//
+                            new AlertDialog.Builder(this)
+                                    .setTitle("درخواست مجوز")
+                                    .setMessage("برای عملکرد صحیح برنامه باید دسترسی به دریافت پیامک تایید شود")
+                                    .setPositiveButton("موافقم", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
 
-//                sendMessage();
-                SMSReceiverImpl smsListener = new SMSReceiverImpl();
-                IntentFilter intentFilter = new IntentFilter();
-                intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
-                registerReceiver(smsListener, intentFilter);
+                                            checkAndRequestPermissions();
 
-            } else {
+                                        }
+                                    })
+                                    .setNegativeButton("لغو", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                Toast.makeText(this, " مجوز رد شد", Toast.LENGTH_SHORT).show();
+                                            dialogInterface.dismiss();
 
+                                        }
+                                    })
+                                    .create()
+                                    .show();
+
+//                            showDialogOK("SMS and Location Services Permission required for this app",
+//                                    new DialogInterface.OnClickListener() {
+//                                        @Override
+//                                        public void onClick(DialogInterface dialog, int which) {
+//                                            switch (which) {
+//                                                case DialogInterface.BUTTON_POSITIVE:
+//                                                    checkAndRequestPermissions();
+//                                                    break;
+//                                                case DialogInterface.BUTTON_NEGATIVE:
+//                                                    // proceed with logic by disabling the related features or quit the app.
+//                                                    break;
+//                                            }
+//                                        }
+//                                    });
+                        }
+                        //permission is denied (and never ask again is  checked)
+                        //shouldShowRequestPermissionRationale will return false
+                        else {
+                            Toast.makeText(this, "Go to settings and enable permissions", Toast.LENGTH_LONG)
+                                    .show();
+                            //                            //proceed with logic by disabling the related features or quit the app.
+                        }
+                    }
+                }
             }
-
         }
+
+//        if (requestCode == SMS_REQUEST_CODE) {
+//
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+////                sendMessage();
+//                SMSReceiverImpl smsListener = new SMSReceiverImpl();
+//                IntentFilter intentFilter = new IntentFilter();
+//                intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+//                registerReceiver(smsListener, intentFilter);
+//
+//            } else {
+//
+//                Toast.makeText(this, " مجوز رد شد", Toast.LENGTH_SHORT).show();
+//
+//            }
+//
+//        }
 
     }
 
