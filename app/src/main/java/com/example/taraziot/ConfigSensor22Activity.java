2c65e7ce736161db8849512a9b0ce0a717d149a3
Your Name <you@example.com>
2022-05-24 13:25:32 +0430

    server and sensor are working properly.

--- app/src/main/java/com/example/taraziot/ConfigSensor22Activity.java
+++ app/src/main/java/com/example/taraziot/ConfigSensor22Activity.java
@@ -48,135 +48,136 @@ public class ConfigSensor22Activity extends AppCompatActivity {
     UserManagerSharedPrefs userManagerSharedPrefs;
     int SERVER_PORT;
     Socket socket;
-    @Override
-    protected void onResume() {
-        super.onResume();
-        userManagerSharedPrefs = new UserManagerSharedPrefs(this);
-        Context context = ConfigSensor22Activity.this.getApplicationContext();
-        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        userManagerSharedPrefs = new UserManagerSharedPrefs(this);
+//        Context context = ConfigSensor22Activity.this.getApplicationContext();
+//        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+//        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+////        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+////        Toast.makeText(context, "first " + ip, Toast.LENGTH_SHORT).show();
+//// cut the last octet from ip (if you want to keep the . at the end, add 1 to the second parameter
+//        String firstThreeOctets = ip.substring(0, ip.lastIndexOf(".")); // 192.168.1
+//
+////        String lastOctet = ip.substring(ip.lastIndexOf(".") + 1); // 97
+////        Toast.makeText(context, "Second " + lastOctet, Toast.LENGTH_SHORT).show();
+//        String newIp = firstThreeOctets + ".1"; // 192.168.1.100
+////        Toast.makeText(context, "Third " + newIp, Toast.LENGTH_SHORT).show();
+////        Toast.makeText(context, "ip: " + ip, Toast.LENGTH_SHORT).show();
+////        SERVER_IP = "192.168.43.111";
+////        SERVER_IP = "192.168.43.111";
+//
+////                SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
+////        SERVER_IP = newIp;
+////        SERVER_IP = "192.168.0.100";
+//
 //        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
-//        Toast.makeText(context, "first " + ip, Toast.LENGTH_SHORT).show();
-// cut the last octet from ip (if you want to keep the . at the end, add 1 to the second parameter
-        String firstThreeOctets = ip.substring(0, ip.lastIndexOf(".")); // 192.168.1
-
-//        String lastOctet = ip.substring(ip.lastIndexOf(".") + 1); // 97
-//        Toast.makeText(context, "Second " + lastOctet, Toast.LENGTH_SHORT).show();
-        String newIp = firstThreeOctets + ".1"; // 192.168.1.100
-//        Toast.makeText(context, "Third " + newIp, Toast.LENGTH_SHORT).show();
-//        Toast.makeText(context, "ip: " + ip, Toast.LENGTH_SHORT).show();
-//        SERVER_IP = "192.168.43.111";
-//        SERVER_IP = "192.168.43.111";
-
-//                SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
-//        SERVER_IP = newIp;
-//        SERVER_IP = "192.168.0.100";
-
-        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
-        currentWifiName = wifiMgr.getConnectionInfo().getSSID(); // SSID Name
-
-        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
-        int ip3 = wifiInfo.getIpAddress();
-//        String ipAddress = Formatter.formatIpAddress(ip3);
-        String ipAddress = "192.168.0.100";
-        int index23 = ipAddress.indexOf(".");
-        deviceIpTrimmed = ipAddress.substring(index23 + 7);
-        SERVER_IP = baseIp + deviceIpTrimmed;
-
-
-//        SERVER_IP =
-//        SERVER_IP = serveredt.getText().toString().trim();
-//        Toast.makeText(context, "in", Toast.LENGTH_SHORT).show();
-        SERVER_PORT = 8888;
-        Thread1 = new Thread(new ConfigSensor22Activity.Thread1());
-        Thread1.start();
-
-//        String testValue = String.valueOf(ipAddress);
-//        btntest2222.setOnClickListener(new View.OnClickListener() {
+//        currentWifiName = wifiMgr.getConnectionInfo().getSSID(); // SSID Name
+//
+//        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+//        int ip3 = wifiInfo.getIpAddress();
+////        String ipAddress = Formatter.formatIpAddress(ip3);
+//        String ipAddress = "192.168.133.1";
+//        int index23 = ipAddress.indexOf(".");
+//        deviceIpTrimmed = ipAddress.substring(index23 + 7);
+//        SERVER_IP = baseIp + deviceIpTrimmed;
+//
+//
+////        SERVER_IP =
+////        SERVER_IP = serveredt.getText().toString().trim();
+////        Toast.makeText(context, "in", Toast.LENGTH_SHORT).show();
+//        SERVER_PORT = 8888;
+//        Thread1 = new Thread(new ConfigSensor22Activity.Thread1());
+//        Thread1.start();
+//
+////        String testValue = String.valueOf(ipAddress);
+////        btntest2222.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Toast.makeText(context, testValue, Toast.LENGTH_SHORT).show();
+////            }
+////        });
+////
+////        btnTest.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Intent intent = new Intent(ConfigSensor22Activity.this, ConfigSensorActivity.class);
+////                startActivity(intent);
+////                finish();
+////            }
+////        });
+//
+//        wfiSettingBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                Toast.makeText(context, testValue, Toast.LENGTH_SHORT).show();
+//                try {
+//                    Intent intent = new Intent(Intent.ACTION_MAIN, null);
+//                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//                    ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.wifi.WifiSettings");
+//                    intent.setComponent(cn);
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    startActivity(intent);
+//                } catch (ActivityNotFoundException ignored) {
+//                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+//                }
 //            }
 //        });
 //
-//        btnTest.setOnClickListener(new View.OnClickListener() {
+//        sendShit.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(ConfigSensor22Activity.this, ConfigSensorActivity.class);
-//                startActivity(intent);
-//                finish();
+//            public void onClick(View v) {
+////               ssidNameTrimmed = userManagerSharedPrefs.getSsidNameForSave().substring(1);
+//                ssidNameTrimmed = userManagerSharedPrefs.getSsidNameForSave();
+////               ssidNameTrimmed = "SRV3658";
+////                if (ssidNameTrimmed.charAt(ssidNameTrimmed.length() - 1) == 'x') {
+////                }
+//
+////                ssidNameTrimmed = ssidNameTrimmed.substring(0, ssidNameTrimmed.length() - 1);
+//
+////                Toast.makeText(context, ssidNameTrimmed, Toast.LENGTH_SHORT).show();
+//
+////ssidNameTrimmed = "YAS2";
+//                String sensorNameConfirmation = currentWifiName;
+//                String serverNameConfirmation = ssidNameTrimmed;
+//                new AlertDialog.Builder(ConfigSensor22Activity.this)
+//                        .setTitle("تایید نهایی")
+//                        .setMessage(" \nاطلاعات سرور " + sensorNameConfirmation + " به سنسور " + serverNameConfirmation + " ارسال شود؟")
+//                        .setPositiveButton("بله", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//
+////                                userManagerSharedPrefs.clearAllInformation();
+////                                Intent intent = new Intent(MainActivity.this, ConfigServerActivity.class);
+////                                startActivity(intent);
+////                                finish();
+//
+////                                System.exit(0);
+//                                mainMessage = "1:" + ssidNameTrimmed + ",2:" + userManagerSharedPrefs.getServerPassword();
+//                                try {
+//                                    new Thread(new Thread3(mainMessage)).start();
+//                                } catch (Exception e) {
+//                                    System.out.println(e.toString());
+//                                    Toast.makeText(context, "مشکلی پیش آمد. یا هنوز به سرور متصل هستید و یا به دستگاه اشتباهی وصل شده اید.", Toast.LENGTH_LONG).show();
+//                                }
+//
+//                            }
+//                        })
+//                        .setNegativeButton("خیر", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                                dialogInterface.dismiss();
+//
+//                            }
+//                        })
+//                        .create()
+//                        .show();
+//
 //            }
 //        });
-
-        wfiSettingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_MAIN, null);
-                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                    ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.wifi.WifiSettings");
-                    intent.setComponent(cn);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                } catch (ActivityNotFoundException ignored) {
-                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-                }
-            }
-        });
-
-        sendShit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//               ssidNameTrimmed = userManagerSharedPrefs.getSsidNameForSave().substring(1);
-                ssidNameTrimmed = userManagerSharedPrefs.getSsidNameForSave();
-//               ssidNameTrimmed = "SRV3658";
-//                if (ssidNameTrimmed.charAt(ssidNameTrimmed.length() - 1) == 'x') {
-//                }
-
-//                ssidNameTrimmed = ssidNameTrimmed.substring(0, ssidNameTrimmed.length() - 1);
-
-//                Toast.makeText(context, ssidNameTrimmed, Toast.LENGTH_SHORT).show();
-
-//ssidNameTrimmed = "YAS2";
-                String sensorNameConfirmation = currentWifiName;
-                String serverNameConfirmation = ssidNameTrimmed;
-                new AlertDialog.Builder(ConfigSensor22Activity.this)
-                        .setTitle("تایید نهایی")
-                        .setMessage(" \nاطلاعات سرور " + sensorNameConfirmation + " به سنسور " + serverNameConfirmation + " ارسال شود؟")
-                        .setPositiveButton("بله", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-//                                userManagerSharedPrefs.clearAllInformation();
-//                                Intent intent = new Intent(MainActivity.this, ConfigServerActivity.class);
-//                                startActivity(intent);
-//                                finish();
-
-//                                System.exit(0);
-                                mainMessage = "1:" + ssidNameTrimmed + ",2:" + userManagerSharedPrefs.getServerPassword();
-                                try {
-                                    new Thread(new Thread3(mainMessage)).start();
-                                } catch (Exception e) {
-                                    System.out.println(e.toString());
-                                    Toast.makeText(context, "مشکلی پیش آمد. یا هنوز به سرور متصل هستید و یا به دستگاه اشتباهی وصل شده اید.", Toast.LENGTH_LONG).show();
-                                }
-
-                            }
-                        })
-                        .setNegativeButton("خیر", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                                dialogInterface.dismiss();
-
-                            }
-                        })
-                        .create()
-                        .show();
-
-            }
-        });
-    }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -213,12 +214,13 @@ public class ConfigSensor22Activity extends AppCompatActivity {
         WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
         int ip3 = wifiInfo.getIpAddress();
 //        String ipAddress = Formatter.formatIpAddress(ip3);
-        String ipAddress = "192.168.0.100";
+        String ipAddress = "192.168.133.1";
         int index23 = ipAddress.indexOf(".");
         deviceIpTrimmed = ipAddress.substring(index23 + 7);
+//        SERVER_IP = baseIp + deviceIpTrimmed;
         SERVER_IP = baseIp + deviceIpTrimmed;
 
-
+        SERVER_IP = "192.168.133.1";
 //        SERVER_IP =
 //        SERVER_IP = serveredt.getText().toString().trim();
 //        Toast.makeText(context, "in", Toast.LENGTH_SHORT).show();
